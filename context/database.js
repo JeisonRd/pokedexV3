0873defed39584a5612d1ead7fb6bc7abdb460a5
jeison <jeisonrafael11@gmail.com>
2024-05-02 23:46:49 -0400

    add env variables

--- context/database.js
+++ context/database.js
@@ -1,9 +1,14 @@
 const Sequelize = require("sequelize");
 
 const port = process.env.PORT || 3306;
-const sequelize = new Sequelize("pokedexV2", "root", "", {
+const host = process.env.DB_HOST || "localhost";
+const user = process.env.USER || "root";
+const password = process.env.PASSWORD || "";
+const DB_NAME = process.env.DB_NAME || "pokedexV2";
+
+const sequelize = new Sequelize(DB_NAME, user, password, {
   dialect: "mysql",
-  host: "localhost",
+  host: host,
   port: port,
 });
 
